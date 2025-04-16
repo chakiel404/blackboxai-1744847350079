bc944c2b780195b345dede51c770f9cf572fd0c9
Muhammad Amanah <73313340+chakiel404@users.noreply.github.com>
2025-04-17 06:50:26 +0700

    Add config/jwt.php 1744847425712

---
+++ config/jwt.php
@@ -0,0 +1,39 @@
+<?php
+class JWT {
+    private static $secret_key = "your-secret-key-here";
+    private static $algorithm = 'HS256';
+
+    public static function encode($payload) {
+        $header = json_encode(['typ' => 'JWT', 'alg' => self::$algorithm]);
+        $payload = json_encode($payload);
+        
+        $base64UrlHeader = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($header));
+        $base64UrlPayload = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($payload));
+        
+        $signature = hash_hmac('sha256', $base64UrlHeader . "." . $base64UrlPayload, self::$secret_key, true);
+        $base64UrlSignature = str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($signature));
+        
+        return $base64UrlHeader . "." . $base64UrlPayload . "." . $base64UrlSignature;
+    }
+
+    public static function decode($jwt) {
+        $parts = explode('.', $jwt);
+        
+        if (count($parts) != 3) {
+            return false;
+        }
+        
+        $header = base64_decode(str_replace(['-', '_'], ['+', '/'], $parts[0]));
+        $payload = base64_decode(str_replace(['-', '_'], ['+', '/'], $parts[1]));
+        $signature = base64_decode(str_replace(['-', '_'], ['+', '/'], $parts[2]));
+        
+        $validSignature = hash_hmac('sha256', $parts[0] . "." . $parts[1], self::$secret_key, true);
+        
+        if (hash_equals($signature, $validSignature)) {
+            return json_decode($payload, true);
+        }
+        
+        return false;
+    }
+}
+?> 
