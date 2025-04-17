153888ba9045bd495ff3333e59c0ec312d1deb70
Muhammad Amanah <73313340+chakiel404@users.noreply.github.com>
2025-04-17 10:27:47 +0700

    Add vendor/composer/installed.php 1744860466872

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elearning/system',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '27b0febd2b932932b5b8038e1ede4b146a9aac3f',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'elearning/system' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '27b0febd2b932932b5b8038e1ede4b146a9aac3f',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tecnickcom/tcpdf' => array(
+            'pretty_version' => '6.9.1',
+            'version' => '6.9.1.0',
+            'reference' => 'ed27e28a4c478f7f4015b5e7e7b1912af9e85f2b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../tecnickcom/tcpdf',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
